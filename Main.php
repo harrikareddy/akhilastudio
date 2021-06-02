c6766ab7d2bc63b7a18282bb447cc8779486cf87
harrikareddy <85228915+harrikareddy@users.noreply.github.com>
2021-06-02 18:11:03 +0530

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form
+
+$name = $_POST['name'];
+$number= $_POST['mobile'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$to = "harrikasoftwarepilla@gmail.com";
+$subject = "Mail From Akhila beauty studio";
+$txt ="Name = ". $name . "\r\n Phone Number=". $number ."\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: akhila@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
